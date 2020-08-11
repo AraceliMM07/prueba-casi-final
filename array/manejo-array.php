3dd1ea2825b3d5afda5eb60bf048fb3cff3170b6
AraceliMM07 <68762136+AraceliMM07@users.noreply.github.com>
2020-08-11 18:26:04 -0500

    Add files via upload

---
+++ array/manejo-array.php
@@ -0,0 +1,10 @@
+<?php 
+
+$datos = array("Araceli Manzanarez", "19", "Tecnologías de la Información", "me gusta","me llama la atención", "las clases ", "pandemia", "en linea", "medio de la television", "tareas", "alumnos", "Zihuatanejo", "estudio", "intereses" , " tareas" , "bien", "surgen ");
+
+echo "Mi nombre es " . $datos[0] . " e vivido toda mi vida en " . $datos[11] . ". " . "Tengo " . $datos[1] . " años y estudio " . $datos[2] . " estudio esta carrera porque " . $datos[3] . " , " . $datos[4]  . " esta carrera" . " aunque  también tengo otros " . $datos[13] . "." ;
+echo " Actualmente " . $datos[5] . "han cambiado por la " . $datos[6] . " pues ". "ahora " . $datos[5] . "son " . $datos[7] . " o por " . $datos[8] . " como en el caso de mi hermana. ". " Para este tiempo ya deberiamos estar acostrumbrados los " .  $datos[10] . " para esta nueva forma de " . $datos[12] . ",  la unica desventaja que noto es que " . $datos[16] . " algunas dudas con respecto a algunas ". $datos[9] . "."  . " Con respecto a las entregas de" . $datos[14] . " en la mayoria voy " . $datos[15] . ".";
+
+
+
+ ?>
